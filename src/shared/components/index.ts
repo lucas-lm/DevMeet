8214456dff8e48d3de05d295f5ad94ef76fdde50
Lucas <lucas-m@outlook.com>
2022-03-06 01:11:52 -0300

    refact: change directory name

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,6 +1,6 @@
 import Title from "./Title/Title";
 import Highlight from './Highlight/Highlight';
 import Paragraph from './Paragraph/Paragraph';
-import TextIconButton from './Button/TextIconButton'
+import TextIconButton from './TextIconButton/TextIconButton'
 
 export { Title, Highlight, Paragraph, TextIconButton }
