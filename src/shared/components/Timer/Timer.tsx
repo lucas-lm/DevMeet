f8b5c73aa97a8bca04ef77668b39be4e8c153be1
Lucas <lucas-m@outlook.com>
2022-03-09 03:30:57 -0300

    feat: add onSecondChange and onMinutesChange call back param to the Timer component

--- src/shared/components/Timer/Timer.tsx
+++ src/shared/components/Timer/Timer.tsx
@@ -9,27 +9,41 @@ interface TimerComponentProps {
   size?: number,
   width?: string,
   direction?: 'row' | 'column',
-  onTimesUp?: (startedAt: string, to: string) => void
+  onTimesUp?: () => void,
+  onMinutesChange?: (reaminingTime: number) => void
+  onSecondsChange?: (reaminingTime: number) => void
 }
 
 const DEFAULT_SIZE = 16*3
 
-const Timer: React.FC<TimerComponentProps> = ({ children, to, dtFormat, size=DEFAULT_SIZE, width, direction='row', onTimesUp }) => {
+const Timer: React.FC<TimerComponentProps> = ({
+  children,
+  to, dtFormat,
+  size=DEFAULT_SIZE,
+  width, direction='row',
+  onTimesUp, onMinutesChange, onSecondsChange
+}) => {
   const date = dtFormat ? moment(to, dtFormat) : moment.utc(to)
-  const startedAt = moment()
-  const [now, setNow] = useState<Moment>(startedAt)
+  const [now, setNow] = useState<Moment>(moment())
   const timeFromNow = (u: 'days' | 'hours' | 'minutes' | 'seconds') => date.diff(now, u)
+
+  const secondsLeft = timeFromNow('seconds')
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
+      onSecondsChange && onSecondsChange(secondsLeft)
       setNow(moment())
     }, 1000)
+
+    if (secondsLeft % 60 === 0) {
+      onMinutesChange && onMinutesChange(secondsLeft)
+    }
 
     return () => clearTimeout(timeoutId)
   }, [now])
 
   if (timeFromNow('seconds') <= 0) {
-    onTimesUp && onTimesUp(startedAt.toString(), to)
+    onTimesUp && onTimesUp()
     return <>{children}</>
   }
 
@@ -38,7 +52,7 @@ const Timer: React.FC<TimerComponentProps> = ({ children, to, dtFormat, size=DEF
       <ClockTimeUnit value={timeFromNow('days')} unit='dias' size={size}/>
       <ClockTimeUnit value={timeFromNow('hours') % 24} unit='horas' size={size}/>
       <ClockTimeUnit value={timeFromNow('minutes') % 60} unit='minutos' size={size}/>
-      <ClockTimeUnit value={timeFromNow('seconds') % 60} unit='segundos' size={size}/>
+      <ClockTimeUnit value={secondsLeft % 60} unit='segundos' size={size}/>
     </TimerRootContainer>
   )
 }
